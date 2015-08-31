c78766497d48073f91c419d7568275a9643313d8
Bryce Cater <brycedcarter@gmail.com>
2015-08-31 11:37:29 -0400

    -started the declaration and implementation of rMotor functions -declared some required power system classes -updated CMakeLists to include .h files

--- motors.cpp
+++ motors.cpp
@@ -3,10 +3,101 @@
 //
 
 #include "rMotors.h"
-#include <stdio.h>
 
-int testMotors(void)
+namespace RVR
 {
-    printf("\n=====================\nThe motors script is working\n======================\n\n");
-    return 0;
+
+    RVR::MotorProperties::MotorProperties(int in1, int in2, int i0, int i1, int i2, int i3, int i4, int fault, int sleep,
+                                          int reset, int decay, RVR::PowerRail *powerRail, int vRef, int rSense)
+    {
+        this->IN1 = in1;
+        this->IN2 = in2;
+        this->I0 = i0;
+        this->I1 = i1;
+        this->I2 = i2;
+        this->I3 = i3;
+        this->I4 = i4;
+        this->FAULT = fault;
+        this->SLEEP = sleep;
+        this->RESET = reset;
+        this->DECAY = decay;
+        this->POWER_RAIL = powerRail;
+        this->V_REF = vRef;
+        this->R_SENSE = rSense;
+        this->I_FULL_SCALE = this->V_REF / (5 * this->R_SENSE) * 1000;
+    }
+
+    RVR::Motor::Motor(RVR::MotorName motorName)
+    {
+        // Assign the correct pin mapping for the motor
+        switch (motorName)
+        {
+            case RVR::DRIVE_MOTOR_1:
+                this->motorProperties = RVR::Motor::drive1MotorMapping;
+            case RVR::DRIVE_MOTOR_2:
+                this->motorProperties = RVR::Motor::drive2MotorMapping;
+            case RVR::TREAT_MOTOR:
+                this->motorProperties = RVR::Motor::treatMotorMapping;
+            case RVR::CAMERA_MOTOR:
+                this->motorProperties = RVR::Motor::cameraMotorMapping;
+        }
+    }
+
+    RVR::PowerRail* motorRail = RVR::PowerManager::getRail(RVR::RAIL12V0);
+    const RVR::MotorProperties * const RVR::Motor::drive1MotorMapping = new RVR::MotorProperties(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, motorRail, 2500, 250);
+    const RVR::MotorProperties * const RVR::Motor::drive2MotorMapping = new RVR::MotorProperties(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, motorRail, 2500, 250);
+    const RVR::MotorProperties * const RVR::Motor::treatMotorMapping = new RVR::MotorProperties(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, motorRail, 2500, 250);
+    const RVR::MotorProperties * const RVR::Motor::cameraMotorMapping = new RVR::MotorProperties(1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, motorRail, 2500, 250);
+
+    int Motor::setCurrentLimit(int currentLimit) {
+        if (currentLimit > this->motorProperties->I_FULL_SCALE)
+        {
+            currentLimit = this->motorProperties->I_FULL_SCALE;
+        }
+        int percentOfFullScale = (currentLimit * 100) / this->motorProperties->I_FULL_SCALE;
+        int fiveBitValue = (percentOfFullScale*100) * 31 / 10000;
+
+        if ((fiveBitValue & (1<<0)) != 0)
+        {
+            // set gpio for I0 to high
+        }
+        else
+        {
+            // set gpio for I0 to low
+        }
+        if ((fiveBitValue & (1<<1)) != 0)
+        {
+            // set gpio for I1 to high
+        }
+        else
+        {
+            // set gpio for I1 to low
+        }
+        if ((fiveBitValue & (1<<2)) != 0)
+        {
+            // set gpio for I2 to high
+        }
+        else
+        {
+            // set gpio for I2 to low
+        }
+        if ((fiveBitValue & (1<<3)) != 0)
+        {
+            // set gpio for I3 to high
+        }
+        else
+        {
+            // set gpio for I3 to low
+        }
+        if ((fiveBitValue & (1<<4)) != 0)
+        {
+            // set gpio for I4 to high
+        }
+        else
+        {
+            // set gpio for I4 to low
+        }
+
+        return 0;
+    }
 }
--- rMotors.h
+++ rMotors.h
@@ -5,6 +5,92 @@
 #ifndef FIRMWARE_MOTORS_H
 #define FIRMWARE_MOTORS_H
 
-int testMotors(void);
+#include <string>
+#include "rPower.h"
 
+namespace RVR
+{
+    enum MotorName
+    {
+        DRIVE_MOTOR_1,
+        DRIVE_MOTOR_2,
+        CAMERA_MOTOR,
+        TREAT_MOTOR
+    };
+
+    class MotorProperties
+    {
+    public:
+        int IN1; // pin value for IN1
+        int IN2; // pin value for IN2
+        int I0; // pin value for I0
+        int I1; // pin value for I1
+        int I2; // pin value for I2
+        int I3; // pin value for I3
+        int I4; // pin value for I4
+        int FAULT; // pin value for FAULT
+        int SLEEP; // pin value for SLEEP
+        int RESET; // pin value for RESET
+        int DECAY; // pin value for DECAY
+        RVR::PowerRail * POWER_RAIL; // pointer to the power rail that the motor runs off of
+        int V_REF; // value (in mV) of the reference voltage
+        int R_SENSE; // value (in mOhm) of the sense resistor
+        int I_FULL_SCALE; // maximum value (in mA) of the chopping current
+
+        MotorProperties(){}
+
+        //! All arguments constructor
+        /*!
+          \param in1 pin value for IN1
+          \param in2 pin value for IN2
+          \param i0 pin value for I0
+          \param i1 pin value for I1
+          \param i2 pin value for I2
+          \param i3 pin value for I3
+          \param i4 pin value for I4
+          \param fault pin value for FAULT
+          \param sleep pin value for SLEEP
+          \param reset pin value for RESET
+          \param decay pin value for DECAY
+          \param powerRail pointer to the power rail that the motor runs off of
+          \param vRef value (in mV) of the reference voltage
+          \param vRef value (in mOhm) of the sense resistor
+        */
+        MotorProperties(int in1, int in2, int i0, int i1, int i2, int i3, int i4, int fault, int sleep, int reset, int decay, RVR::PowerRail * powerRail, int vRef, int rSense);
+    };
+
+
+    class Motor
+    {
+    public:
+        Motor(){}
+        Motor(MotorName motorName);
+
+        //! Sets the current limit of the motor
+        /*!
+          \param current limit (in mA)
+        */
+        int setCurrentLimit(int currentLimit);
+
+    protected:
+        const MotorProperties *motorProperties;
+
+        static const MotorProperties * const drive1MotorMapping;
+        static const MotorProperties * const drive2MotorMapping;
+        static const MotorProperties * const treatMotorMapping;
+        static const MotorProperties * const cameraMotorMapping;
+
+    };
+
+    class StepperMotor : public Motor
+    {
+
+    };
+
+    class DcMotor : public Motor
+    {
+
+    };
+
+}
 #endif //FIRMWARE_MOTORS_H
